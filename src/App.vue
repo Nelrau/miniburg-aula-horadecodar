51202e80a164dfdea562b3a8a97934752e7cdeff
Nelrau <nelrau@gmail.com>
2024-09-19 04:32:40 -0300

    nav e footer

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div>
+ <Navbar :logo="logo_src" :alt="app_name" />
+ <router-view />
+ <Footer />
+ </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from './components/Footer.vue';
+import Navbar from './components/Navbar.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default{
+  components:{
+    Navbar,
+    Footer,
+  },
+  data(){
+    return{
+      logo_src: "/img/logo.png",
+      app_name: "logo"
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+*{
+  font-family: Helvetica;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.main-container{
+  margin: 50px;
+  min-height:370px;
+}
+h1{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <footer id="footer">
+            <p>Make your burger &copy;2024</p>
+        </footer>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+<style>
+#footer{
+    height: 100px;
+    background-color: #222;
+    border-top:4px solid #111;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fcba03;
+    bottom: 0;
+    width:100%;
+}
+</style>
